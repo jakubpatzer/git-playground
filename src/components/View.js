5a424bee1ca9d2c989fc05c92ce7d37342a88f57
jakubpatzer <jakub.patzer@protonmail.com>
2019-05-29 16:52:14 +0200

    created View component

---
+++ src/components/View.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class View extends Component {
+  render() {
+    return (
+      <div>
+        This is View Component content.
+      </div>
+    )
+  }
+}
+
+export default View
